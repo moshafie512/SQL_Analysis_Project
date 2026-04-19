f4c12ad0232a5cbed1484506b69fb9fe9eb00275
Mohamed Elshafie <moshafie512@gmail.com>
2026-04-19 14:22:07 +0200

    Add files via upload

---
+++ Data Cleaning.sql
@@ -0,0 +1,199 @@
+# Data Cleaning
+
+
+SELECT *
+FROM layoffs;
+
+# 1. Reomve Duplicates
+# 2. Stanardize the Data
+# 3. Null Values or Blank Values
+# 4. Remove Any Columns
+
+
+
+CREATE TABLE layoffs_staging
+LIKE layoffs;
+
+
+SELECT *
+FROM layoffs_staging;
+
+
+INSERT layoffs_staging
+SELECT *
+FROM layoffs;
+
+#___________________________________________________________
+
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, industry, total_laid_off,percentage_laid_off, `date`) AS row_num
+FROM layoffs_staging;
+
+
+WITH duplicates_cte AS
+(
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, industry, total_laid_off,percentage_laid_off, `date`, stage,
+country, funds_raised_millions) AS row_num
+FROM layoffs_staging
+)
+SELECT *
+FROM duplicates_cte
+WHERE row_num > 1;
+
+
+SELECT *
+FROM layoffs_staging
+WHERE company = 'Casper';
+
+
+# This is a wrong step when deleting duplicates
+WITH duplicates_cte AS
+(
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, industry, total_laid_off,percentage_laid_off, `date`, stage,
+country, funds_raised_millions) AS row_num
+FROM layoffs_staging
+)
+DELETE
+FROM duplicates_cte
+WHERE row_num > 1;
+
+
+CREATE TABLE `layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` int
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+SELECT *
+FROM layoffs_staging2
+WHERE row_num > 1;
+
+INSERT INTO layoffs_staging2
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, industry, total_laid_off,percentage_laid_off, `date`, stage,
+country, funds_raised_millions) AS row_num
+FROM layoffs_staging;
+
+
+
+DELETE
+FROM layoffs_staging2
+WHERE row_num > 1;
+
+SELECT *
+FROM layoffs_staging2;
+
+#_______________________________________________________
+# Standardizing data
+
+SELECT company, TRIM(company)
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET company = TRIM(company);
+-- ------------------------------------------
+SELECT DISTINCT industry
+FROM layoffs_staging2
+;
+
+UPDATE layoffs_staging2
+SET industry = 'Crypto'
+WHERE industry LIKE 'Crypto%';
+
+-- ------------------------------------------
+
+SELECT DISTINCT country, TRIM(TRAILING '.' FROM country)
+FROM layoffs_staging2
+ORDER BY 1;
+
+UPDATE layoffs_staging2
+SET country = TRIM(TRAILING '.' FROM country) 
+WHERE country LIKE 'United States%';
+
+-- ------------------------------------------
+
+SELECT `date`
+FROM layoffs_staging2;
+
+
+UPDATE layoffs_staging2
+SET `date` = STR_TO_DATE(`date`, '%m/%d/%Y');
+
+
+ALTER TABLE layoffs_staging2
+MODIFY COLUMN `date` DATE;
+
+-- ------------------------------------------
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+UPDATE layoffs_staging2
+SET industry = NULL
+WHERE industry = '';
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry IS NULL
+OR industry = '';
+
+SELECT *
+FROM layoffs_staging2
+WHERE company LIKE 'Bally%';
+
+SELECT t1.industry, t2.industry
+FROM layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company
+    AND t1.location = t2.location
+WHERE (t1.industry IS NULL OR t1.industry = '')
+AND t2.industry IS NOT NULL;
+
+UPDATE layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company
+SET t1.industry = t2.industry
+WHERE t1.industry IS NULL
+AND t2.industry IS NOT NULL;
+
+-- ------------------------------------------
+
+SELECT *
+FROM layoffs_staging2;
+
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+
+DELETE
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+-- ------------------------------------------
+
+# THE FINAL CLEANED DATA
+SELECT *
+FROM layoffs_staging2;
+
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
+
