3ba24742bb2df3c1dcc13317fe56fe37e95958c8
Mohamed Elshafie <moshafie512@gmail.com>
2026-04-19 14:42:23 +0200

    EDA.sql

--- EDA.sql
+++ EDA.sql
@@ -1,53 +1,56 @@
 # Exploratory Data Analysis (EDA)
 
+-- View Entire Dataset
 SELECT *
 FROM layoffs_staging2;
 
 
+-- Maximum Values in Dataset
 SELECT MAX(total_laid_off), MAX(percentage_laid_off)
 FROM layoffs_staging2;
 
+-- Companies with 100% Layoffs
 SELECT *
 FROm layoffs_staging2
 WHERE percentage_laid_off = 1
 ORDER BY funds_raised_millions DESC;
 
+-- Total Layoffs per Company
 SELECT company, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY company
 ORDER BY 2 DESC;
 
+-- Dataset Time Range
 SELECT MIN(`date`), MAX(`date`)
 FROM layoffs_staging2;
 
+-- Total Layoffs by Country
 SELECT country, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY country
 ORDER BY 2 DESC;
 
-
-SELECT *
-FROM layoffs_staging2;
-
+-- Total Layoffs by Year
 SELECT YEAR(`date`), SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY YEAR(`date`)
 ORDER BY 1 DESC;
 
-
+-- Average Layoff Percentage per Company
 SELECT company, AVG(percentage_laid_off)
 FROM layoffs_staging2
 GROUP BY company
 ORDER BY 2 DESC;
 
-
+-- Monthly Layoffs Trend
 SELECT SUBSTRING(`date`,1,7) AS `MONTH`, SUM(total_laid_off)
 FROM layoffs_staging2
 WHERE SUBSTRING(`date`,1,7) IS NOT NULL
 GROUP BY `MONTH`
 ORDER BY 1 ASC;
 
-
+-- Rolling Total of Layoffs (Cumulative Trend)
 WITH Rolling_Total AS
 (
 SELECT SUBSTRING(`date`,1,7) AS `MONTH`, SUM(total_laid_off) AS total_off
@@ -61,13 +64,14 @@ SELECT `MONTH`, total_off
 FROM Rolling_Total;
 
 
-
+-- Company Layoffs by Year
 SELECT company,YEAR(`date`), SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY company, YEAR(`date`)
 ORDER BY 3 DESC;
 
 
+-- Top 5 Companies with Highest Layoffs per Year
 WITH Company_Year(company, years, total_laid_off) AS
 (
 SELECT company,YEAR(`date`), SUM(total_laid_off)
@@ -83,9 +87,5 @@ WHERE years IS NOT NULL
 SELECT *
 FROM Company_Year_Rank
 WHERE Ranking < 6;
-
-
-
-
 
 
