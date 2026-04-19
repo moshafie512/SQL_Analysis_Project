a19de00074bd30d4bc09139827430d0c13f77baf
Mohamed Elshafie <moshafie512@gmail.com>
2026-04-19 14:53:13 +0200

    Data Cleaning.sql

--- Data Cleaning.sql
+++ Data Cleaning.sql
@@ -1,16 +1,16 @@
-# Data Cleaning
+-- Data Cleaning
 
 
 SELECT *
 FROM layoffs;
 
-# 1. Reomve Duplicates
-# 2. Stanardize the Data
-# 3. Null Values or Blank Values
-# 4. Remove Any Columns
+-- 1. Reomve Duplicates
+-- 2. Stanardize the Data
+-- 3. Null Values or Blank Values
+-- 4. Remove Any Columns
 
 
-
+-- Create Staging Table
 CREATE TABLE layoffs_staging
 LIKE layoffs;
 
@@ -18,19 +18,21 @@ LIKE layoffs;
 SELECT *
 FROM layoffs_staging;
 
-
+-- Copy Original Data into Staging
 INSERT layoffs_staging
 SELECT *
 FROM layoffs;
 
-#___________________________________________________________
+-- ___________________________________________________________
 
+-- Identify Duplicates
 SELECT *,
 ROW_NUMBER() OVER(
 PARTITION BY company, industry, total_laid_off,percentage_laid_off, `date`) AS row_num
 FROM layoffs_staging;
 
 
+-- Detect Full Duplicate Records (CTE)
 WITH duplicates_cte AS
 (
 SELECT *,
@@ -43,26 +45,7 @@ SELECT *
 FROM duplicates_cte
 WHERE row_num > 1;
 
-
-SELECT *
-FROM layoffs_staging
-WHERE company = 'Casper';
-
-
-# This is a wrong step when deleting duplicates
-WITH duplicates_cte AS
-(
-SELECT *,
-ROW_NUMBER() OVER(
-PARTITION BY company, industry, total_laid_off,percentage_laid_off, `date`, stage,
-country, funds_raised_millions) AS row_num
-FROM layoffs_staging
-)
-DELETE
-FROM duplicates_cte
-WHERE row_num > 1;
-
-
+-- Create Clean Table to Add row_num Column
 CREATE TABLE `layoffs_staging2` (
   `company` text,
   `location` text,
@@ -76,10 +59,12 @@ CREATE TABLE `layoffs_staging2` (
   `row_num` int
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+-- Identify Duplicates
 SELECT *
 FROM layoffs_staging2
 WHERE row_num > 1;
 
+-- Insert Data with Row Numbers
 INSERT INTO layoffs_staging2
 SELECT *,
 ROW_NUMBER() OVER(
@@ -87,17 +72,17 @@ PARTITION BY company, industry, total_laid_off,percentage_laid_off, `date`, stag
 country, funds_raised_millions) AS row_num
 FROM layoffs_staging;
 
-
-
+-- Delete Duplicates
 DELETE
 FROM layoffs_staging2
 WHERE row_num > 1;
 
+-- The New Table without Duplicates
 SELECT *
 FROM layoffs_staging2;
 
-#_______________________________________________________
-# Standardizing data
+-- _______________________________________________________
+-- Standardizing data
 
 SELECT company, TRIM(company)
 FROM layoffs_staging2;
